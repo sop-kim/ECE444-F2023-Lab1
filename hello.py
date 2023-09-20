47e3acadad2fafdd3ddfbc080755fcfa9bdeb7f8
sop-kim <77212828+sop-kim@users.noreply.github.com>
2023-09-19 22:16:56 -0400

    add email field

--- hello.py
+++ hello.py
@@ -3,9 +3,10 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Regexp
 
 from datetime import datetime
+import re
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'some secret string'
@@ -15,9 +16,15 @@ moment = Moment(app)
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+    name = StringField('What is your name?', 
+                       validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', 
+                        validators=[DataRequired(),
+                                    Email(message="Please include @ in the email address"),
+                                    Regexp(r"^.*utoronto.*$",message="Please use your UofT email")])
     submit = SubmitField('Submit')
-    
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -34,12 +41,22 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        # handle name
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+
+        # handle email
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['email'] = form.email.data
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, 
+                           name=session.get('name'), 
+                           email=session.get('email'))
 
 
 @app.route('/user/<name>')
